ebc5c7efd365befef9542063690a4b539587e8ff
zyl82311 <59716638+zyl82311@users.noreply.github.com>
2023-04-27 17:40:12 +0800

    Create liangjihui.js

---
+++ liangjihui.js
@@ -0,0 +1,11 @@
+var 13852811157 = JSON.parse($response.body);
+
+  13852811157.data = {
+    "is_pay" : true,
+    "mobile" : "13852811157",
+    "experience_card" : true,
+    "is_member" : true,
+    "end_time" : 4092599349
+  };
+
+$done({body : JSON.stringify(13852811157)});
