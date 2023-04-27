bfb7bca63610fc4192101536a1f1fb3fcd91f001
zyl82311 <59716638+zyl82311@users.noreply.github.com>
2023-04-27 17:46:47 +0800

    Update liangjihui.js

--- liangjihui.js
+++ liangjihui.js
@@ -1,3 +1,21 @@
+/*************************************
+
+项目名称：靓机汇
+下载地址：https://t.cn/A6NMLjKA
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/guapi\.liangjihui\.com\/front\/user\/memberInfo url script-response-body https://raw.githubusercontent.com/zyl82311/1/main/liangjihui.js
+
+[mitm]
+hostname = guapi.liangjihui.com
+
+*************************************/
+
 var 13852811157 = JSON.parse($response.body);
 
   13852811157.data = {
