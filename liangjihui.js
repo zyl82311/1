c52173d45947b7bacc8a1c21dce5da5a61803458
zyl82311 <59716638+zyl82311@users.noreply.github.com>
2023-05-31 08:20:43 +0800

    Update liangjihui.js

--- liangjihui.js
+++ liangjihui.js
@@ -1,29 +1,29 @@
 /*************************************
 
 项目名称：靓机汇
-下载地址：https://t.cn/A6NMLjKA
 脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https?:\/\/guapi\.liangjihui\.com\/front\/user\/memberInfo url script-response-body https://raw.githubusercontent.com/zyl82311/1/main/liangjihui.js
+# 解锁会员信息
+^https?:\/\/guapi\.liangjihui\.com\/front\/(quote\/look.+|user\/memberInfo) url script-response-body https://raw.githubusercontent.com/zyl82311/1/main/liangjihui.js
+# 屏蔽顶部滚屏
+^https:\/\/ljh\.dianxiaoman\.com\/ljh\/api\/home\/getHomeList url reject
 
 [mitm]
-hostname = guapi.liangjihui.com
+hostname = guapi.liangjihui.com, ljh.dianxiaoman.com
 
 *************************************/
 
-var 13852811157 = JSON.parse($response.body);
 
-  13852811157.data = {
-    "is_pay" : true,
-    "mobile" : "13852811157",
-    "experience_card" : true,
-    "is_member" : true,
-    "end_time" : 4092599349
-  };
+var chxm1023 = JSON.parse($response.body);
 
-$done({body : JSON.stringify(13852811157)});
+  chxm1023.data.is_pay = true;
+  chxm1023.data.experience_card = true;
+  chxm1023.data.is_member = true;
+  chxm1023.data.end_time = 4092599349;
+  chxm1023.data.is_see = true;
+
+$done({body : JSON.stringify(chxm1023)});
